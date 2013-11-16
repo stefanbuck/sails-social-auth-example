0a28e71b8e5c21616e470d56ee3a40233da770b1
naman34 <naman34@gmail.com>
2013-11-17 02:02:51 +0530

    Added conditional INT typecasting for correct login
    The User model saves the 'uid' as an integer, services often send the profile id as string. ParseInt should work on its own, but for best compatibility and to account for possible changes to the User model this should be the best way to find if the user exists already in the model.

--- config/middleware.js
+++ config/middleware.js
@@ -6,7 +6,14 @@ var passport = require('passport')
 var verifyHandler = function (token, tokenSecret, profile, done) {
     process.nextTick(function () {
 
-        User.findOne({uid: profile.id}).done(function (err, user) {
+        User.findOne(
+                {
+                    or : [
+                            {uid: parseInt(profile.id)}, 
+                            {uid: profile.id}
+                        ]
+                }
+            ).done(function (err, user) {
             if (user) {
                 return done(null, user);
             } else {
