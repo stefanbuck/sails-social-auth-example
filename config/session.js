352aff7518dda02145f687d2f076ab34b94b6076
Stefan Buck <web@broud.de>
2014-08-24 19:03:59 +0200

    Reimplement session handling

--- config/session.js
+++ config/session.js
@@ -67,11 +67,11 @@ module.exports.session = {
   *                                                                          *
   ***************************************************************************/
 
-  // adapter: 'mongo',
-  // host: 'localhost',
-  // port: 27017,
-  // db: 'sails',
-  // collection: 'sessions',
+  adapter: 'mongo',
+  host: 'localhost',
+  port: 27017,
+  db: 'sails-social-auth',
+  collection: 'sessions',
 
   /***************************************************************************
   *                                                                          *
